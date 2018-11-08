a1e590ff6ed3fad411297d61ff38c3a662c72eca
shashwat1002 <singhshashwat1002@gmail.com>
2018-11-08 10:36:24 +0530

    make stylistic changes

--- openwisp_controller/config/migrations/0015_add_relevant_permissions_to_admin_and_operators.py
+++ openwisp_controller/config/migrations/0015_add_relevant_permissions_to_admin_and_operators.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Permission
 from django.contrib.auth.management import create_permissions 
 from django.db import transaction
 
+
 def make_default_permissions_in_code(apps, schema_editor):
     for app_config in apps.get_app_configs():
         app_config.models_module = True 
@@ -21,29 +22,27 @@ def make_default_permissions_in_code(apps, schema_editor):
 
 def assignPerm(apps, schema_editor): 
     Group= apps.get_model('openwisp_users', 'Group')
-    admin = Group.objects.get(name="Administrator")
-    operator = Group.objects.get(name="Operator")
-    operators_and_admins_can_change=["device", "config", "template", "location", "floorplan",]
-    operators_read_only_admins_manage = ["vpn", "ca", "cert",]
-    manage_operations = ["add", "change", "delete"]
+    admin = Group.objects.get(name='Administrator')
+    operator = Group.objects.get(name='Operator')
+    operators_and_admins_can_change=['device', 'config', 'template', 'location', 'floorplan',]
+    operators_read_only_admins_manage = ['vpn', 'ca', 'cert',]
+    manage_operations = ['add', 'change', 'delete']
 
-    for i in operators_and_admins_can_change:
-        for j in manage_operations:
-            permission=Permission.objects.get(codename="{}_{}".format(j,i))
-
+    for modelClass in operators_and_admins_can_change:
+        for operation in manage_operations:
+            permission=Permission.objects.get(codename='{}_{}'.format(operation, modelClass))
             admin.permissions.add(permission.pk)
             operator.permissions.add(permission.pk)
             
-
-    for i in operators_read_only_admins_manage:
+    for modelClass in operators_read_only_admins_manage:
         try:
-            permission=Permission.objects.get(codename="view_{}".format(i))
+            permission=Permission.objects.get(codename="view_{}".format(modelClass))
             operator.permissions.add(permission.pk )
         except Permission.DoesNotExist:
             pass   
         
-        for j in manage_operations: 
-            admin.permissions.add(Permission.objects.get(codename="{}_{}".format(j,i)).pk, )
+        for operation in manage_operations: 
+            admin.permissions.add(Permission.objects.get(codename="{}_{}".format(operation,modelClass)).pk, )
     
 
 class Migration(migrations.Migration): 
@@ -51,16 +50,13 @@ class Migration(migrations.Migration):
         ('openwisp_users', '__first__'),
         ('openwisp_users', '0004_default_groups'),
         ('pki', '__first__'),
-  #      ('django_loci', '__first__'),
+#        ('django_loci', '__first__'),
         ('config', '0014_device_hardware_id'),
         
     ]
 
     operations=[
-        migrations.RunPython(make_default_permissions_in_code),
-        migrations.RunPython(assignPerm),
+        migrations.RunPython(make_default_permissions_in_code, reverse_code=migrations.RunPython.noop),
+        migrations.RunPython(assignPerm, reverse_code=migrations.RunPython.noop),
+
     ]
-
-
-
-
